a7e23b1e40d3487c8c0e5fdb53a93290c8f8d195
Jay Han <jayhan.name@gmail.com>
2025-12-23 17:46:10 +0100

    Image

---
+++ app/src/main/java/name/jayhan/pebble/MainActivity.kt
@@ -0,0 +1,154 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package name.jayhan.pebble
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import name.jayhan.pebble.ui.theme.PebbleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                stringResource(R.string.app_title),
+                                fontSize = 32.sp
+                            )
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                Pebble(Modifier.padding(innerPadding))
+            }
+        }
+    }
+}
+
+@Composable
+fun Section(text: String = "") {
+    Text(
+        text = text,
+        fontSize = 32.sp,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun Pebble(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+    ) {
+        Watchface()
+        Box(Modifier.height(8.dp))
+        PermissionsChecked()
+        Box(Modifier.height(8.dp))
+        AwayTimezone()
+        Box(Modifier.height(8.dp))
+        NotificationsList()
+        Box(Modifier.height(8.dp))
+        BluetoothDevices()
+    }
+}
+
+@Composable
+fun Watchface(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Section("Connected")
+        Image(
+            painter = painterResource(R.drawable.screenshot),
+            modifier = modifier.height(168.dp).padding(8.dp).fillMaxWidth(),
+            contentScale = ContentScale.Fit,
+            contentDescription = "Screenshot",
+        )
+    }
+}
+
+@Composable
+fun PermissionsChecked(modifier: Modifier = Modifier) {
+    Section("Permissions")
+}
+
+@Composable
+fun AwayTimezone(modifier: Modifier = Modifier) {
+    var tz by remember { mutableStateOf("8.0") }
+
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "Away ",
+            fontSize = 32.sp
+        )
+        OutlinedTextField(
+            value = tz,
+            onValueChange = {tz = it},
+            modifier = modifier.width(120.dp),
+            textStyle = TextStyle(fontSize = 32.sp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true,
+        )
+        Text(
+            text = " hours",
+            fontSize = 32.sp
+        )
+    }
+}
+
+@Composable
+fun NotificationsList(modifier: Modifier = Modifier) {
+    Section("Notifications")
+}
+
+@Composable
+fun BluetoothDevices(modifier: Modifier = Modifier) {
+    Section("Bluetooth")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PebblePreview() {
+    Pebble()
+}
